8d8de7d06e3b6b8654ce90592c177ab6f7cafeb6
ybahae <ybahaerarslan@gmail.com>
2024-04-20 03:39:25 +0200

    fix webhook url

--- app/(dashboard)/dashboard/create-link/actions.ts
+++ app/(dashboard)/dashboard/create-link/actions.ts
@@ -73,7 +73,10 @@ const processImage = async (file: File) => {
     throw new Error("Insufficient balance2");
   }
 
-  const callbackURL = `https://heavy-donuts-guess.loca.lt/webhooks/replicate`;
+  const callbackURL =
+    process.env.NODE_ENV === "production"
+      ? "https://retro-photo.vercel.app/webhooks/replicate"
+      : `https://heavy-donuts-guess.loca.lt/webhooks/replicate`;
 
   // describe image first
 
